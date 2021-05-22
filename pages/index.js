ab361c1565d4ee7cd6f98c8283b9c2e2609d447d
Joseph Ryan <joseph.ryan.iv@gmail.com>
2021-05-22 13:28:14 -0700

    Homepage carousel image, new content, and appbar icon

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,136 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import {
+  Box,
+  Button,
+  Carousel,
+  Collapsible,
+  Grommet,
+  Heading,
+  Image,
+  Layer,
+  ResponsiveContext,
+  Text,
+} from 'grommet'
+import { FormClose, Menu } from 'grommet-icons'
+import { useState } from 'react'
+
+const theme = {
+  global: {
+    colors: {
+      brand: '',
+    },
+    font: {
+      family: 'Play',
+      // size: '18px',
+      // height: '20px',
+    },
+    p: {
+      border: 0,
+    }
+  },
+};
+
+
+
+
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='xsmall'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
 
 export default function Home() {
+  const [showSidebar, setShowSidebar] = useState(false)
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Grommet theme={theme} themeMode='dark' full>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <>
+          <Head>
+          <title>J&J Landscaping</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet" />
+        </Head>
+          <Box fill>
+            <AppBar>
+              <Heading level='3' margin='none'>J&J Landscaping</Heading>
+              <Button
+                icon={<Menu />}
+                onClick={() => setShowSidebar(!showSidebar)}
+              />
+            </AppBar>
+            <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+              <Box flex align='center' style={{ lineHeight: 1,}}>
+                <Box height="medium" width="large" overflow="hidden">
+                  <Carousel play={4000} fill>
+                    <Image fit="cover" src="/BackyardPics/IMG-8149.jpg" />
+                    <Image fit="cover" src="/BackyardPics/IMG-8153.jpg" />
+                    <Image fit="cover" src="/BackyardPics/IMG-8154.jpg" />
+                    <Image fit="cover" src="/BackyardPics/IMG-8157.jpg" />
+                  </Carousel>
+                </Box>
+                <Box className='homepage-content'>
+                  J&J has over 20 years of experience designing, building, and maintaning residential landscapes.<br/>
+                  We specialize in patios, pavers, concrete, irrigation systems, plants, trees, rocks, boulders,<br/>
+                  new lawns, firepits, landscape lighting, decorative landscaping rocks, and many other items.<br/>
+                  Call or text for free consult and estimate<br/>
+                </Box>
+              </Box>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+                {(!showSidebar || size !== 'small') ? (
+                    <Collapsible direction='horizontal' open={showSidebar}>
+                    <Box
+                      flex
+                      width='medium'
+                      background='light-2'
+                      elevation='small'
+                      align='center'
+                      justify='center'
+                    >
+                      sidebar
+                    </Box>
+                  </Collapsible>
+                ): (
+                  <Layer>
+                    <Box
+                      background='light-2'
+                      tag='header'
+                      align='center'
+                      justify='end'
+                      direction='row'
+                    >
+                      <Button
+                        icon={<FormClose />}
+                        onClick={() => setShowSidebar(false)}
+                      />
+                    </Box>
+                    <Box
+                      fill
+                      background='light-2'
+                      align='center'
+                      justify='center'
+                    >
+                      sidebar
+                    </Box>
+                  </Layer>
+                )}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+            </Box>
+          </Box>
+          </>
+        )}
+      </ResponsiveContext.Consumer>
+    </Grommet>
   )
 }
